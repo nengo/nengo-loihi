Fixed weight scaling problems; ensemble now works

nengo_loihi/loihi_api.py
@@ -53,6 +53,10 @@ class Board(object):
 
         self.probe_map = {}
 
+    def validate(self):
+        for chip in self.chips:
+            chip.validate()
+
     def _add_chip(self, chip):
         assert chip not in self.chips
         self.chip_idxs[chip] = len(self.chips)
@@ -121,6 +125,10 @@ class Chip(object):
         self.cores = []
         self.core_idxs = {}
 
+    def validate(self):
+        for core in self.cores:
+            core.validate()
+
     def _add_core(self, core):
         assert core not in self.cores
         self.core_idxs[core] = len(self.cores)
@@ -156,6 +164,21 @@ class Core(object):
     @property
     def board(self):
         return self.chip.board
+
+    def validate(self):
+        for cxProfile in self.cxProfiles:
+            cxProfile.validate(core=self)
+        for vthProfile in self.vthProfiles:
+            vthProfile.validate(core=self)
+        for synapseFmt in self.synapseFmts:
+            if synapseFmt is not None:
+                synapseFmt.validate(core=self)
+        for synapses in self.synapse_axons:
+            synapseFmt = self.get_synapse_fmt(synapses)
+            idxbits = synapseFmt.realIdxBits
+            for i in synapses.indices:
+                assert np.all(i >= 0)
+                assert np.all(i < 2**idxbits)
 
     def iterate_groups(self):
         i0 = 0
@@ -222,8 +245,15 @@ class Core(object):
         a1 = a0 + axons.n_axons
         self.axon_axons[axons] = (a0, a1)
 
+    def get_synapse_fmt(self, synapses):
+        return self.synapseFmts[self.synapse_fmt_idxs[synapses]]
+
 
 class CxProfile(object):
+    DECAY_U_MAX = 2**12 - 1
+    DECAY_V_MAX = 2**12 - 1
+    REFRACT_DELAY_MAX = 2**6 - 1
+
     params = ('decayU', 'decayV', 'refDelay')
 
     def __init__(self, decayV, decayU, refDelay):
@@ -238,8 +268,15 @@ class CxProfile(object):
     def __hash__(self):
         return hash(tuple(self.__dict__[key] for key in self.params))
 
+    def validate(self, core=None):
+        assert 0 <= self.decayU <= self.DECAY_U_MAX
+        assert 0 <= self.decayV <= self.DECAY_V_MAX
+        assert 0 <= self.refDelay <= self.REFRACT_DELAY_MAX
+
 
 class VthProfile(object):
+    VTH_MAX = 2**17 - 1
+
     params = ('vth',)
 
     def __init__(self, vth):
@@ -252,34 +289,39 @@ class VthProfile(object):
     def __hash__(self):
         return hash(tuple(self.__dict__[key] for key in self.params))
 
+    def validate(self, core=None):
+        assert 0 < self.vth <= self.VTH_MAX
+        # if core is not None:
+        #     assert self.realVth < core.dendrite_shared_cfg.v_max
+
 
 class SynapseFmt(object):
     INDEX_BITS_MAP = [0, 6, 7, 8, 9, 10, 11, 12]
 
-    def __init__(self, WgtLimitMant=0, WgtLimitExp=0, WgtExp=0, DiscMaxWgt=0,
-                 LearningCfg=3, TagBits=0, DlyBits=0, WgtBits=7,
-                 ReuseSynData=0, NumSynapses=63, CIdxOffset=0, CIdxMult=0,
-                 SkipBits=0, IdxBits=5, SynType=0, FanoutType=0,
-                 Compression=0, StdpProfile=0, IgnoreDly=0):
-        self.WgtLimitMant = WgtLimitMant
-        self.WgtLimitExp = WgtLimitExp
-        self.WgtExp = WgtExp
-        self.DiscMaxWgt = DiscMaxWgt
-        self.LearningCfg = LearningCfg
-        self.TagBits = TagBits
-        self.DlyBits = DlyBits
-        self.WgtBits = WgtBits
-        self.ReuseSynData = ReuseSynData
-        self.NumSynapses = NumSynapses
-        self.CIdxOffset = CIdxOffset
-        self.CIdxMult = CIdxMult
-        self.SkipBits = SkipBits
-        self.IdxBits = IdxBits
-        self.SynType = SynType
-        self.FanoutType = FanoutType
-        self.Compression = Compression
-        self.StdpProfile = StdpProfile
-        self.IgnoreDly = IgnoreDly
+    def __init__(self, wgtLimitMant=0, wgtLimitExp=0, wgtExp=0, discMaxWgt=0,
+                 learningCfg=0, tagBits=0, dlyBits=0, wgtBits=0,
+                 reuseSynData=0, numSynapses=0, cIdxOffset=0, cIdxMult=0,
+                 skipBits=0, idxBits=0, synType=0, fanoutType=0,
+                 compression=0, stdpProfile=0, ignoreDly=0):
+        self.wgtLimitMant = wgtLimitMant
+        self.wgtLimitExp = wgtLimitExp
+        self.wgtExp = wgtExp
+        self.discMaxWgt = discMaxWgt
+        self.learningCfg = learningCfg
+        self.tagBits = tagBits
+        self.dlyBits = dlyBits
+        self.wgtBits = wgtBits
+        self.reuseSynData = reuseSynData
+        self.numSynapses = numSynapses
+        self.cIdxOffset = cIdxOffset
+        self.cIdxMult = cIdxMult
+        self.skipBits = skipBits
+        self.idxBits = idxBits
+        self.synType = synType
+        self.fanoutType = fanoutType
+        self.compression = compression
+        self.stdpProfile = stdpProfile
+        self.ignoreDly = ignoreDly
 
     def set(self, **kwargs):
         for key, value in kwargs.items():
@@ -287,28 +329,33 @@ class SynapseFmt(object):
             setattr(self, key, value)
 
     def validate(self, core=None):
-        assert -7 <= self.WgtExp <= 7
-        assert 0 <= self.TagBits < 4
-        assert 0 <= self.DlyBits < 8
-        assert 0 <= self.WgtBits < 8
-        assert 0 <= self.CIdxOffset < 16
-        assert 0 <= self.CIdxMult < 16
-        assert 0 <= self.IdxBits < 8
+        assert -7 <= self.wgtExp <= 7
+        assert 0 <= self.tagBits < 4
+        assert 0 <= self.dlyBits < 8
+        assert 1 <= self.wgtBits < 8
+        assert 0 <= self.cIdxOffset < 16
+        assert 0 <= self.cIdxMult < 16
+        assert 0 <= self.idxBits < 8
 
     @property
     def width(self):
-        return 1 + self.WgtBits
+        return 1 + self.wgtBits
 
     @property
     def isMixed(self):
-        # return 0
-        return self.FanoutType == 1
+        return self.fanoutType == 1
 
     @property
     def Wscale(self):
-        return 14 - self.width + self.WgtExp + self.isMixed
-        # ^ = 8 - width(SYN_l) + 6 + wgtExp(SYN_l) + isMixed(SYN_l)
+        return 6 + self.wgtExp
 
     @property
     def realIdxBits(self):
-        return self.INDEX_BITS_MAP[self.IdxBits]
+        return self.INDEX_BITS_MAP[self.idxBits]
+
+    def discretize_weights(self, w, dtype=np.int32):
+        s = 8 - self.width + self.isMixed
+        m = 2**(8 - s) - 1
+        w = np.round(w / 2.**s).clip(-m, m).astype(dtype)
+        np.left_shift(w, self.Wscale + s, out=w)
+        return w

nengo_loihi/loihi_cx.py
@@ -127,15 +127,15 @@ class CxGroup(object):
         if len(w_maxs) > 0:
             w_maxi = np.argmax(w_maxs)
             w_max = w_maxs[w_maxi]
-            w_scale = (127. / w_max)
+            w_scale = (255. / w_max)
 
-            self.synapses[w_maxi].format(WgtExp=0)
+            self.synapses[w_maxi].format(wgtExp=0)
             synapse_fmt = self.synapses[w_maxi].synapse_fmt
 
             s_scale = 1. / (u_scale * v_scale)
 
             for wgtExp in range(7, -8, -1):
-                synapse_fmt.set(WgtExp=wgtExp)
+                synapse_fmt.set(wgtExp=wgtExp)
                 x_scale = s_scale * w_scale * 2**synapse_fmt.Wscale
                 b_scale = x_scale * v_scale
                 vth = np.round(self.vth * x_scale)
@@ -170,9 +170,9 @@ class CxGroup(object):
             assert dWgtExp >= 0
             wgtExp2 = max(wgtExp - dWgtExp, -7)
             dWgtExp = wgtExp - wgtExp2
-            synapse.format(WgtExp=wgtExp2)
+            synapse.format(wgtExp=wgtExp2)
             for w in synapse.weights:
-                discretize(w, w * w_scale * 2**synapse.synapse_fmt.Wscale)
+                discretize(w, synapse_fmt.discretize_weights(w * w_scale))
 
 
 class CxSynapses(object):
@@ -190,6 +190,12 @@ class CxSynapses(object):
         self.weights = [w.astype(np.float32) for w in weights]
         self.indices = [np.arange(w.size) for w in weights]
         assert weights.shape[0] == self.n_axons
+
+        idxBits = int(np.ceil(np.log2(max(i.max() for i in self.indices) + 1)))
+        idxBits = next(i for i, v in enumerate(SynapseFmt.INDEX_BITS_MAP)
+                       if v >= idxBits)
+        self.format(compression=3, idxBits=idxBits, fanoutType=1,
+                    numSynapses=63, wgtBits=7)
 
     def format(self, **kwargs):
         if self.synapse_fmt is None:
@@ -330,7 +336,7 @@ class CxSimulator(object):
             assert (self.scaleU == 1).all()
             assert (self.scaleV == 1).all()
             self.decayU_fn = (
-                lambda x, u: decay_int(x, u, d=self.decayU, s=self.scaleU))
+                lambda x, u: decay_int(x, u, d=self.decayU, s=self.scaleU, b=1))
             self.decayV_fn = (
                 lambda x, u: decay_int(x, u, d=self.decayV, s=self.scaleV))
         elif group_dtype == np.float32:

nengo_loihi/loihi_interface.py
@@ -41,8 +41,8 @@ def build_core(n2core, core):
         n2core.cxProfileCfg[i].configure(
             decayV=cxProfile.decayV,
             decayU=cxProfile.decayU,
-            bapAction=1,
             refractDelay=cxProfile.refDelay,
+            bapAction=1,
         )
 
     for i, vthProfile in enumerate(core.vthProfiles):
@@ -54,13 +54,25 @@ def build_core(n2core, core):
         if synapseFmt is None:
             continue
 
-        n2core.synapseFmt[i].wgtExp = synapseFmt.WgtExp
-        n2core.synapseFmt[i].wgtBits = synapseFmt.WgtBits
-        n2core.synapseFmt[i].numSynapses = synapseFmt.NumSynapses
-        n2core.synapseFmt[i].idxBits = synapseFmt.IdxBits
-        n2core.synapseFmt[i].compression = synapseFmt.Compression
-        n2core.synapseFmt[i].fanoutType = synapseFmt.FanoutType
-        # ^ TODO: other parameters
+        n2core.synapseFmt[i].wgtLimitMant = synapseFmt.wgtLimitMant
+        n2core.synapseFmt[i].wgtLimitExp = synapseFmt.wgtLimitExp
+        n2core.synapseFmt[i].wgtExp = synapseFmt.wgtExp
+        n2core.synapseFmt[i].discMaxWgt = synapseFmt.discMaxWgt
+        n2core.synapseFmt[i].learningCfg = synapseFmt.learningCfg
+        n2core.synapseFmt[i].tagBits = synapseFmt.tagBits
+        n2core.synapseFmt[i].dlyBits = synapseFmt.dlyBits
+        n2core.synapseFmt[i].wgtBits = synapseFmt.wgtBits
+        n2core.synapseFmt[i].reuseSynData = synapseFmt.reuseSynData
+        n2core.synapseFmt[i].numSynapses = synapseFmt.numSynapses
+        n2core.synapseFmt[i].cIdxOffset = synapseFmt.cIdxOffset
+        n2core.synapseFmt[i].cIdxMult = synapseFmt.cIdxMult
+        n2core.synapseFmt[i].skipBits = synapseFmt.skipBits
+        n2core.synapseFmt[i].idxBits = synapseFmt.idxBits
+        n2core.synapseFmt[i].synType = synapseFmt.synType
+        n2core.synapseFmt[i].fanoutType = synapseFmt.fanoutType
+        n2core.synapseFmt[i].compression = synapseFmt.compression
+        n2core.synapseFmt[i].stdpProfile = synapseFmt.stdpProfile
+        n2core.synapseFmt[i].ignoreDly = synapseFmt.ignoreDly
 
     # TODO: allocator should be checking that vmin, vmax are the same
     #   for all groups on a core
@@ -80,17 +92,17 @@ def build_core(n2core, core):
         delayBits=3)
     # ^ DelayBits=3 allows 1024 Cxs per core
 
+    n_cx = 0
     for group, (i0, i1) in core.iterate_groups():
         build_group(n2core, core, group, i0, i1)
+        n_cx = max(n_cx, i1)
+
+    n2core.numUpdates.configure(numUpdates=n_cx//4 + 1)
 
 
 def build_group(n2core, core, group, i0, i1):
     assert group.scaleU is False
     assert group.scaleV is False
-
-    # for i in range(256):
-    #     n2core.cxMetaState[i].configure(
-    #         phase0=2, phase1=2, phase2=2, phase3=2)
 
     for i, bias in enumerate(group.bias):
         bman, bexp = bias_to_manexp(bias)
@@ -103,14 +115,6 @@ def build_group(n2core, core, group, i0, i1):
 
         phasex = 'phase%d' % (ii % 4,)
         n2core.cxMetaState[ii//4].configure(**{phasex: 2})
-        # if ii % 4 == 0:
-        #     n2core.cxMetaState[ii//4].configure(phase0=2)
-        # elif ii % 4 == 2:
-        #     n2core.cxMetaState[ii//4].configure(phase1=2)
-        # elif ii % 4 == 3:
-        #     n2core.cxMetaState[ii//4].configure(phase2=2)
-        # else:
-        #     n2core.cxMetaState[ii//4].configure(phase3=2)
 
     for synapses in group.synapses:
         build_synapses(n2core, core, group, i0, i1, synapses)
@@ -121,8 +125,6 @@ def build_group(n2core, core, group, i0, i1):
     for probe in group.probes:
         build_probe(n2core, core, group, i0, i1, probe)
 
-    n2core.numUpdates.configure(numUpdates=1)
-
 
 def build_synapses(n2core, core, group, i0, i1, synapses):
     a0, a1 = core.synapse_axons[synapses]
@@ -132,9 +134,11 @@ def build_synapses(n2core, core, group, i0, i1, synapses):
 
     s0 = core.synapse_entries[synapses][0]
     for a in range(a1 - a0):
-        wa = synapses.weights[a]
+        wa = synapses.weights[a] // 2**synapses.synapse_fmt.Wscale
         ia = synapses.indices[a]
+        assert len(wa) == len(ia)
 
+        assert np.all(wa <= 255) and np.all(wa >= -255), str(wa)
         for k, (w, i) in enumerate(zip(wa, ia)):
             n2core.synapses[s0+k].CIdx = i0 + i
             assert n2core.synapses[s0+k].CIdx < i1
@@ -150,8 +154,10 @@ def build_synapses(n2core, core, group, i0, i1, synapses):
 
 def build_axons(n2core, core, group, i0, i1, axons):
     a0, a1 = core.axon_axons[axons]
+    assert a1 - a0 == axons.n_axons
 
     # for now, all axons are one compartment to one axon
+    assert group.n == axons.n_axons
     for i in range(group.n):
         n2core.axonMap[i0+i].configure(ptr=a0+i, len=1)
 