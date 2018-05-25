75a3faa2ddbfcb6a133ee032501f2959ff1172ae
Eric Hunsberger <erichuns@gmail.com>
2018-05-25 10:36:37 -0400

    Changes to test_examples
    - Simulator fixture is now NumpySimulator (for now)
    - test_ens_ens shows ensemble output instead of voltages
    - added test_oscillator
    - removed test_node_ens_ens for now since it doesn't work

--- nengo_loihi/conftest.py
+++ nengo_loihi/conftest.py
@@ -7,4 +7,4 @@ import nengo_loihi
 @pytest.fixture(scope="session")
 def Simulator(request):
     """Simulator class to be used in tests"""
-    return nengo_loihi.Simulator
+    return nengo_loihi.NumpySimulator
--- nengo_loihi/tests/test_examples.py
+++ nengo_loihi/tests/test_examples.py
@@ -1,52 +1,86 @@
+import pytest
+
+import numpy as np
+
 import nengo
 
 
-def test_ens_ens(Simulator, plt):
-    with nengo.Network(seed=1) as model:
+@pytest.mark.parametrize('weights', [False, True])
+def test_ens_ens(Simulator, seed, plt, weights):
+    solver = nengo.solvers.LstsqL2(weights=weights)
+
+    with nengo.Network(seed=seed) as model:
         a = nengo.Ensemble(100, 1, label='a',
                            max_rates=nengo.dists.Uniform(100, 120),
                            intercepts=nengo.dists.Uniform(-0.5, 0.5))
-        avp = nengo.Probe(a.neurons[:5], 'voltage')
-
-        b = nengo.Ensemble(101, 1, label='b',
-                           max_rates=nengo.dists.Uniform(100, 120),
-                           intercepts=nengo.dists.Uniform(-0.5, 0.5))
-        nengo.Connection(a, b, function=lambda x: x + 0.5)
-        bvp = nengo.Probe(b.neurons[:5], 'voltage')
-
-    with Simulator(model, target='sim') as sim:
-        sim.run(0.1)
-
-    print(sim.data[avp])
-    print(sim.data[bvp])
-
-    plt.subplot(211)
-    plt.plot(sim.trange(), sim.data[avp][:, :10])
-    plt.subplot(212)
-    plt.plot(sim.trange(), sim.data[bvp][:, :10])
-
-
-def test_node_ens_ens(Simulator, plt):
-    with nengo.Network(seed=1) as model:
-        u = nengo.Node(output=0.5)
-        up = nengo.Probe(u)
-
-        a = nengo.Ensemble(100, 1, label='a',
-                           max_rates=nengo.dists.Uniform(100, 120),
-                           intercepts=nengo.dists.Uniform(-0.5, 0.5))
-        nengo.Connection(u, a, synapse=None)
         ap = nengo.Probe(a)
 
         b = nengo.Ensemble(101, 1, label='b',
                            max_rates=nengo.dists.Uniform(100, 120),
                            intercepts=nengo.dists.Uniform(-0.5, 0.5))
-        nengo.Connection(a, b)
+        nengo.Connection(a, b, function=lambda x: x + 0.5, solver=solver)
         bp = nengo.Probe(b)
 
-    with Simulator(model, target='sim') as sim:
-        sim.run(0.5)
+    with Simulator(model) as sim:
+        sim.run(1.0)
 
     output_filter = nengo.synapses.Alpha(0.02)
-    plt.plot(sim.trange(), output_filter.filtfilt(sim.data[up]))
     plt.plot(sim.trange(), output_filter.filtfilt(sim.data[ap]))
     plt.plot(sim.trange(), output_filter.filtfilt(sim.data[bp]))
+
+
+# def test_node_ens_ens(Simulator, plt):
+#     with nengo.Network(seed=1) as model:
+#         u = nengo.Node(output=0.5)
+#         up = nengo.Probe(u)
+
+#         a = nengo.Ensemble(100, 1, label='a',
+#                            max_rates=nengo.dists.Uniform(100, 120),
+#                            intercepts=nengo.dists.Uniform(-0.5, 0.5))
+#         nengo.Connection(u, a, synapse=None)
+#         ap = nengo.Probe(a)
+
+#         b = nengo.Ensemble(101, 1, label='b',
+#                            max_rates=nengo.dists.Uniform(100, 120),
+#                            intercepts=nengo.dists.Uniform(-0.5, 0.5))
+#         nengo.Connection(a, b)
+#         bp = nengo.Probe(b)
+
+#     with Simulator(model) as sim:
+#         sim.run(0.5)
+
+#     output_filter = nengo.synapses.Alpha(0.02)
+#     plt.plot(sim.trange(), output_filter.filtfilt(sim.data[up]))
+#     plt.plot(sim.trange(), output_filter.filtfilt(sim.data[ap]))
+#     plt.plot(sim.trange(), output_filter.filtfilt(sim.data[bp]))
+
+
+@pytest.mark.parametrize('weights', [False, True])
+def test_oscillator(Simulator, seed, plt, weights):
+    solver = nengo.solvers.LstsqL2(weights=weights)
+    tau = 0.1
+    alpha = 1.0
+
+    def f(x):
+        x0, x1 = x
+        r = np.sqrt(x0**2 + x1**2)
+        a = np.arctan2(x1, x0)
+        dr = -(r - 1)
+        da = alpha
+        r = r + tau*dr
+        a = a + tau*da
+        return [r*np.cos(a), r*np.sin(a)]
+
+    with nengo.Network(seed=seed) as model:
+        a = nengo.Ensemble(200, 2, label='a',
+                           max_rates=nengo.dists.Uniform(100, 120),
+                           intercepts=nengo.dists.Uniform(-0.5, 0.5))
+        ap = nengo.Probe(a)
+
+        nengo.Connection(a, a, function=f, synapse=tau, solver=solver)
+
+    with Simulator(model) as sim:
+        sim.run(4.0)
+
+    output_filter = nengo.synapses.Alpha(0.02)
+    plt.plot(sim.trange(), output_filter.filtfilt(sim.data[ap]))
