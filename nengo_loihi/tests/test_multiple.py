36669b1e71e6fde1386dbcd00866b9598299e9f9
Terry Stewart <terry.stewart@appliedbrainresearch.com>
2018-06-28 01:51:43 -0400

    Added test for multiple spike inputs

---
+++ nengo_loihi/tests/test_multiple.py
@@ -0,0 +1,25 @@
+import nengo
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize('precompute', [True, False])
+def test_multiple_stims(Simulator, seed, precompute):
+    with nengo.Network(seed=seed) as model:
+        N = 10
+        a = nengo.Ensemble(N, 1, seed=seed)
+        b = nengo.Ensemble(N, 1, seed=seed)
+        nengo.Connection(nengo.Node(0.5), a)
+        nengo.Connection(nengo.Node(0.5), b)
+
+        p_a = nengo.Probe(a)
+        p_b = nengo.Probe(b)
+    with Simulator(model, precompute=precompute) as sim:
+        sim.run(0.1)
+
+    # Note: these should ideally be identical,
+    #  but noise in the spiking interneurons will
+    #  make them different.  If spiking decoders are
+    #  implemented, then these should be identical.
+    assert np.allclose(np.mean(sim.data[p_a]),
+                       np.mean(sim.data[p_b]), atol=0.05)
