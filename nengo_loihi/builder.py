Fix dt scaling on connections

nengo_loihi/builder.py
@@ -443,25 +443,28 @@ def build_connection(model, conn):
         if isinstance(conn.pre_obj, splitter.ChipReceiveNeurons):
             weights = transform
         else:
+            # input is on-off neuron encoded, so double/flip transform
             weights = np.column_stack([transform, -transform])
-            # ^ input is on-off neuron encoded, so double/flip transform
-            weights = weights / (INTER_RATE * INTER_N)
-            # ^ remove rate factor added by pre-compute nodes
 
+            # remove rate factor added by pre-compute nodes;
+            # (max_rate = INTER_RATE * INTER_N) is the spike rate we
+            # use to represent a value of +/- 1
+            weights = weights / (INTER_RATE * INTER_N * model.dt)
     elif (isinstance(conn.pre_obj, Ensemble) and
           isinstance(conn.pre_obj.neuron_type, Direct)):
         raise NotImplementedError()
     elif isinstance(conn.pre_obj, Ensemble):  # Normal decoded connection
         eval_points, weights, solver_info = model.build(
             conn.solver, conn, rng, transform)
+
+        # the decoder solver assumes a spike height of 1/dt; that isn't the
+        # case on loihi, so we need to undo that scaling
         weights = weights / model.dt
-        # ^ scale, since nengo spikes have 1/dt scaling
 
         if not conn.solver.weights:
             needs_interneurons = True
     else:
         assert conn.pre_slice == slice(None)
-        # assert conn.post_slice == slice(None)
         assert transform.ndim == 2
         weights = transform
 
@@ -564,7 +567,7 @@ def build_connection(model, conn):
 
             syn = CxSynapses(n1)
             gain = model.params[conn.post_obj.ensemble].gain
-            syn.set_full_weights(weights.T * gain / model.dt)
+            syn.set_full_weights(weights.T * gain)
             post_cx.add_synapses(syn)
             model.objs[conn]['weights'] = syn
 

nengo_loihi/tests/test_ens_on_host.py
@@ -2,7 +2,6 @@ import numpy as np
 import pytest
 
 import nengo
-from nengo.dists import Uniform, UniformHypersphere
 import nengo_loihi
 
 
@@ -13,7 +12,7 @@ def test_ens_decoded_on_host(precompute, allclose, Simulator, seed, plt):
     with nengo.Network(seed=seed) as model:
         nengo_loihi.add_params(model)
 
-        stim = nengo.Node(lambda t: [np.sin(t*2*np.pi)])
+        stim = nengo.Node(lambda t: [np.sin(t * 2 * np.pi)])
 
         a = nengo.Ensemble(100, 1)
         model.config[a].on_chip = False
@@ -40,90 +39,48 @@ def test_ens_decoded_on_host(precompute, allclose, Simulator, seed, plt):
 
 
 @pytest.mark.parametrize('precompute', [True, False])
-def test_ens_neurons_on_host(precompute, allclose, Simulator, seed, plt, rng):
+def test_ens_neurons_on_host(precompute, allclose, Simulator, seed, plt):
     out_synapse = nengo.synapses.Alpha(0.03)
 
     n = 50
-    transform = 0.0125 * np.eye(n)
-
-    # Sample the distributions early so `a` and `b` have the same params
-    ens_params = dict(
-        encoders=UniformHypersphere(surface=True).sample(n, d=1, rng=rng),
-        max_rates=Uniform(100, 120).sample(n, rng=rng),
-        intercepts=Uniform(-0.5, 0.5).sample(n, rng=rng),
-    )
 
     with nengo.Network(seed=seed) as model:
         nengo_loihi.add_params(model)
 
-        stim = nengo.Node(lambda t: [np.sin(t*2*np.pi)])
+        stim = nengo.Node(lambda t: [np.sin(t * 2 * np.pi)])
 
-        a = nengo.Ensemble(n, 1, **ens_params)
+        a = nengo.Ensemble(n, 1)
         model.config[a].on_chip = False
         nengo.Connection(stim, a)
 
-        b = nengo.Ensemble(n, 1, **ens_params)
-        nengo.Connection(a.neurons, b.neurons, transform=transform)
+        b = nengo.Ensemble(n, 1)
+        nengo.Connection(a.neurons, b.neurons, transform=np.eye(n))
+
+        c = nengo.Node(size_in=1)
+        nengo.Connection(b, c)
 
         p_stim = nengo.Probe(stim, synapse=out_synapse)
         p_a = nengo.Probe(a, synapse=out_synapse)
         p_b = nengo.Probe(b, synapse=out_synapse)
+        p_c = nengo.Probe(c, synapse=out_synapse)
 
     with Simulator(model, precompute=precompute) as sim:
         sim.run(1.0)
 
-    plt.plot(sim.trange(), sim.data[p_stim])
-    plt.plot(sim.trange(), sim.data[p_a])
-    plt.plot(sim.trange(), sim.data[p_b])
+    with model:
+        model.config[a].on_chip = True
 
-    assert allclose(sim.data[p_b], sim.data[p_a], atol=0.15, rtol=0.15)
-
-
-@pytest.mark.parametrize('precompute', [True, False])
-def test_post_node_on_host(precompute, allclose, Simulator, seed, plt, rng):
-    out_synapse = nengo.synapses.Alpha(0.03)
-
-    n = 50
-    transform = 0.0125 * np.eye(n)
-
-    neuron_type = nengo.LIF()
-    encoders = UniformHypersphere(surface=True).sample(n, d=1, rng=rng)
-    max_rates = Uniform(100, 120).sample(n, rng=rng)
-    intercepts = Uniform(-0.5, 0.5).sample(n, rng=rng)
-    ens_params = dict(
-        encoders=encoders, max_rates=max_rates, intercepts=intercepts,
-        neuron_type=neuron_type)
-
-    solver = nengo.solvers.LstsqL2()
-    eval_points = UniformHypersphere(surface=False).sample(1000, d=1, rng=rng)
-    gain, bias = neuron_type.gain_bias(max_rates, intercepts)
-    A = neuron_type.rates(np.dot(eval_points, encoders.T), gain, bias)
-    D, _ = solver(A, eval_points)
-
-    with nengo.Network(seed=seed) as model:
-        nengo_loihi.add_params(model)
-
-        stim = nengo.Node(lambda t: [np.sin(t*2*np.pi)])
-
-        a = nengo.Ensemble(n, 1, **ens_params)
-        model.config[a].on_chip = False
-        nengo.Connection(stim, a)
-
-        b = nengo.Ensemble(n, 1, **ens_params)
-        nengo.Connection(a.neurons, b.neurons, transform=transform)
-
-        v = nengo.Node(size_in=1)
-        nengo.Connection(b.neurons, v, transform=D.T)
-
-        p_stim = nengo.Probe(stim, synapse=out_synapse)
-        p_a = nengo.Probe(a, synapse=out_synapse)
-        p_b = nengo.Probe(b, synapse=out_synapse)
-
-    with Simulator(model, precompute=precompute) as sim:
-        sim.run(1.0)
+    with Simulator(model, precompute=precompute) as sim2:
+        sim2.run(1.0)
 
     plt.plot(sim.trange(), sim.data[p_stim])
     plt.plot(sim.trange(), sim.data[p_a])
     plt.plot(sim.trange(), sim.data[p_b])
+    plt.plot(sim.trange(), sim.data[p_c])
+    plt.plot(sim2.trange(), sim2.data[p_a])
+    plt.plot(sim2.trange(), sim2.data[p_b])
+    plt.plot(sim2.trange(), sim.data[p_c])
 
-    assert allclose(sim.data[p_b], sim.data[p_a], atol=0.15, rtol=0.15)
+    assert allclose(sim.data[p_a], sim2.data[p_a], atol=0.15)
+    assert allclose(sim.data[p_b], sim2.data[p_b], atol=0.15)
+    assert allclose(sim.data[p_c], sim2.data[p_c], atol=0.15)