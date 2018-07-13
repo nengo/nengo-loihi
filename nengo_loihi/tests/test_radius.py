bd90bbec76d4a2c5db39f2ca91f27f6ab2ecb8df
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2018-07-13 10:48:14 -0400

    Use weight parameter in radius test

--- nengo_loihi/tests/test_radius.py
+++ nengo_loihi/tests/test_radius.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.mark.parametrize('radius', [0.01, 1, 100])
-def test_radius_probe(Simulator, seed, plt, radius):
+def test_radius_probe(Simulator, seed, radius):
     with nengo.Network(seed=seed) as model:
         stim = nengo.Node(radius / 2.)
         ens = nengo.Ensemble(n_neurons=100, dimensions=1,
@@ -21,7 +21,7 @@ def test_radius_probe(Simulator, seed, plt, radius):
 @pytest.mark.parametrize('radius1', [0.01, 100])
 @pytest.mark.parametrize('radius2', [0.01, 100])
 @pytest.mark.parametrize('weights', [True, False])
-def test_radius_ens_ens(Simulator, seed, plt, radius1, radius2, weights):
+def test_radius_ens_ens(Simulator, seed, radius1, radius2, weights):
     with nengo.Network(seed=seed) as model:
         stim = nengo.Node(radius1 / 2.)
         a = nengo.Ensemble(n_neurons=100, dimensions=1,
@@ -32,7 +32,7 @@ def test_radius_ens_ens(Simulator, seed, plt, radius1, radius2, weights):
                            intercepts=nengo.dists.Uniform(-0.95, 0.95))
         nengo.Connection(stim, a, synapse=None)
         nengo.Connection(a, b, synapse=0.01, transform=radius2 / radius1,
-                         solver=nengo.solvers.LstsqL2(weights=True))
+                         solver=nengo.solvers.LstsqL2(weights=weights))
         p = nengo.Probe(b, synapse=0.1)
     with Simulator(model, precompute=True) as sim:
         sim.run(0.4)
