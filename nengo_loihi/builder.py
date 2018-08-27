Fix dt scaling fix.
I added a test for ensemble to neuron connections, which failed
because the weights were scaled up by 1/dt. However, the node to
neuron connections were scaled properly, so what ended up working
was to revert the previous reverting of the dt scaling.
However, doing this caused `test_n2n_on_host` to fail when the pre
ensemble was simulated off chip and the data sent into the post
neuron which were on chip, so to fix that the transform when
pre is a `ChipReceiveNeurons` object is now scaled by 1/dt.

nengo_loihi/builder.py
@@ -480,14 +480,14 @@ def build_connection(model, conn):
 
         assert transform.shape[1] == conn.pre.size_out
         if isinstance(conn.pre_obj, splitter.ChipReceiveNeurons):
-            weights = transform
+            weights = transform / model.dt
         else:
             # input is on-off neuron encoded, so double/flip transform
             weights = np.column_stack([transform, -transform])
 
             # (max_rate = INTER_RATE * INTER_N) is the spike rate we
             # use to represent a value of +/- 1
-            weights = weights / (INTER_RATE * INTER_N)
+            weights = weights / (INTER_RATE * INTER_N * model.dt)
     elif (isinstance(conn.pre_obj, Ensemble) and
           isinstance(conn.pre_obj.neuron_type, nengo.Direct)):
         raise NotImplementedError()
@@ -506,7 +506,7 @@ def build_connection(model, conn):
     elif isinstance(conn.pre_obj, Neurons):
         assert conn.pre_slice == slice(None)
         assert transform.ndim == 2
-        weights = transform
+        weights = transform / model.dt
         neuron_type = conn.pre_obj.ensemble.neuron_type
     else:
         raise NotImplementedError("Connection from type %r" % (
@@ -615,7 +615,7 @@ def build_connection(model, conn):
 
             syn = CxSynapses(n1)
             gain = model.params[conn.post_obj.ensemble].gain
-            syn.set_full_weights(weights.T * gain / model.dt)
+            syn.set_full_weights(weights.T * gain)
             post_cx.add_synapses(syn)
             model.objs[conn]['weights'] = syn
 