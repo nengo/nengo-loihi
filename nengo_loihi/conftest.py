08304b46324fdb24547e99b34d27c13f6cee7d94
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2018-09-13 16:57:24 -0300

    Add --no-hang option to pytest
    Automatically skips tests that cause Loihi to hang, so that the whole test suite can be run uninterrupted

--- nengo_loihi/conftest.py
+++ nengo_loihi/conftest.py
@@ -36,6 +36,18 @@ def pytest_terminal_summary(terminalreporter):
             np.mean(all_rmses), np.std(all_rmses)))
 
 
+def pytest_addoption(parser):
+    parser.addoption("--no-hang", action="store_true", default=False,
+                     help="Skip tests that hang")
+
+
+def pytest_runtest_setup(item):
+    if (getattr(item.obj, "hang", False) and
+            item.config.getvalue("--target") == "loihi" and
+            item.config.getvalue("--no-hang")):
+        pytest.xfail("This test causes Loihi to hang indefinitely")
+
+
 @pytest.fixture(scope="session")
 def Simulator(request):
     """Simulator class to be used in tests"""
