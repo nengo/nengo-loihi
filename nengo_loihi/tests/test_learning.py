b9636131bb9d83789bd7ea8661a8b338b6ad152e
Eric Hunsberger <erichuns@gmail.com>
2018-11-05 11:04:36 -0500

    squash! Added comparative PES learning test
    - Added comparison to Nengo to standard PES learning test.
    - Also simplified the PES learning test so that it no longer requires
      fitting, it just makes sure that the final learning output is near
      the target.
    - This replaces the comparative test.

--- nengo_loihi/tests/test_learning.py
+++ nengo_loihi/tests/test_learning.py
@@ -84,6 +84,7 @@ def test_pes_comm_channel(allclose, plt, seed, Simulator, n_per_dim, dims):
     scale = np.linspace(1, 0, dims + 1)[:-1]
     input_fn = lambda t: np.sin(t * 2 * np.pi) * scale
 
+    tau = 0.01
     with nengo.Network(seed=seed) as model:
         stim = nengo.Node(input_fn)
 
@@ -94,7 +95,7 @@ def test_pes_comm_channel(allclose, plt, seed, Simulator, n_per_dim, dims):
         conn = nengo.Connection(
             pre, post,
             function=lambda x: np.zeros(dims),
-            synapse=0.01,
+            synapse=tau,
             learning_rule_type=nengo.PES(learning_rate=1e-3))
 
         nengo.Connection(post, conn.learning_rule)
@@ -104,34 +105,38 @@ def test_pes_comm_channel(allclose, plt, seed, Simulator, n_per_dim, dims):
         p_pre = nengo.Probe(pre, synapse=0.02)
         p_post = nengo.Probe(post, synapse=0.02)
 
-    with Simulator(model, precompute=False) as sim:
-        sim.run(5.0)
+    simtime = 5.0
+    with nengo.Simulator(model) as nengo_sim:
+        nengo_sim.run(simtime)
 
-    t = sim.trange()
+    with Simulator(model, precompute=False) as loihi_sim:
+        loihi_sim.run(simtime)
+
+    t = nengo_sim.trange()
+    pre_tmask = t > 0.1
+    post_tmask = t > simtime - 1.0
+
+    inter_tau = loihi_sim.model.inter_tau
+    y = nengo_sim.data[p_stim]
+    y_dpre = nengo.Lowpass(inter_tau).filt(y)
+    y_dpost = nengo.Lowpass(tau).combine(nengo.Lowpass(inter_tau)).filt(y_dpre)
+    y_nengo = nengo_sim.data[p_post]
+    y_loihi = loihi_sim.data[p_post]
+
     plt.subplot(211)
-    plt.plot(t, sim.data[p_stim])
-    plt.plot(t, sim.data[p_pre])
-    plt.plot(t, sim.data[p_post])
-
-    # --- fit input_fn to output, determine magnitude
-    #     The larger the magnitude, the closer the output is to the input
-    x = np.array([input_fn(tt)[0] for tt in t[t > 4]])
-    y = sim.data[p_post][t > 4][:, 0]
-    m = np.linspace(0, 1, 21)
-    errors = np.abs(y - m[:, None]*x).mean(axis=1)
-    m_best = m[np.argmin(errors)]
+    plt.plot(t, y_dpost, 'k', label='target')
+    plt.plot(t, y_nengo, 'b', label='nengo')
+    plt.plot(t, y_loihi, 'g', label='loihi')
 
     plt.subplot(212)
-    plt.plot(t[t > 4], x)
-    plt.plot(t[t > 4], y)
-    plt.plot(t[t > 4], m_best * x, ':')
+    plt.plot(t[post_tmask], y_loihi[post_tmask] - y_dpost[post_tmask], 'k')
+    plt.plot(t[post_tmask], y_loihi[post_tmask] - y_nengo[post_tmask], 'b')
 
-    assert allclose(sim.data[p_pre][t > 0.1],
-                    sim.data[p_stim][t > 0.1],
-                    atol=0.15,
-                    rtol=0.15)
-    assert np.min(errors) < 0.3, "Not able to fit correctly"
-    assert m_best > (0.3 if n_per_dim < 150 else 0.6)
+    assert allclose(loihi_sim.data[p_pre][pre_tmask], y_dpre[pre_tmask],
+                    atol=0.1, rtol=0.05)
+    assert allclose(y_loihi[post_tmask], y_dpost[post_tmask],
+                    atol=0.1, rtol=0.05)
+    assert allclose(y_loihi, y_nengo, atol=0.15, rtol=0.1)
 
 
 def test_multiple_pes(allclose, plt, seed, Simulator):
@@ -166,71 +171,3 @@ def test_multiple_pes(allclose, plt, seed, Simulator):
 
     for i, target in enumerate(targets):
         assert allclose(sim.data[probe][t > 0.8, i], target, atol=0.05)
-
-
-@pytest.mark.skipif(pytest.config.getoption("--target") != "loihi",
-                    reason="need Loihi as comparison")
-@pytest.mark.parametrize('n_per_dim', [120, 200])
-@pytest.mark.parametrize('dims', [1, 3])
-def test_pes_comm_channel_compare(allclose, plt, seed, Simulator, n_per_dim, dims):
-    scale = np.linspace(1, 0, dims + 1)[:-1]
-    input_fn = lambda t: np.sin(t * 2 * np.pi) * scale
-
-    with nengo.Network(seed=seed) as model:
-        stim = nengo.Node(input_fn)
-
-        pre = nengo.Ensemble(n_per_dim * dims, dims)
-        post = nengo.Node(size_in=dims)
-
-        nengo.Connection(stim, pre, synapse=None)
-        conn = nengo.Connection(
-            pre, post,
-            function=lambda x: np.zeros(dims),
-            synapse=0.01,
-            learning_rule_type=nengo.PES(learning_rate=1e-3))
-
-        error = nengo.Node(size_in=dims)
-        nengo.Connection(post, error)
-        nengo.Connection(stim, error, transform=-1)
-        nengo.Connection(error, conn.learning_rule)
-
-        p_stim = nengo.Probe(stim, synapse=0.02)
-        p_pre = nengo.Probe(pre, synapse=0.02)
-        p_post = nengo.Probe(post, synapse=0.02)
-
-    simtime = 2.0
-
-    with nengo.Simulator(model) as nengo_sim:
-        nengo_sim.run(simtime)
-
-    with nengo_loihi.Simulator(model, target='sim') as emulator_sim:
-        emulator_sim.run(simtime)
-
-    with Simulator(model, precompute=False) as loihi_sim:
-        loihi_sim.run(simtime)
-
-    t = emulator_sim.trange()
-
-    plt.subplot(311)
-    plt.plot(t, emulator_sim.data[p_stim], label='target')
-    plt.plot(t, emulator_sim.data[p_pre], label='sim pre')
-    plt.plot(t, emulator_sim.data[p_post], label='sim post')
-    plt.legend(loc=1)
-
-    plt.subplot(312)
-    plt.plot(t, loihi_sim.data[p_stim], label='target')
-    plt.plot(t, loihi_sim.data[p_pre], label='loihi pre')
-    plt.plot(t, loihi_sim.data[p_post], label='loihi post')
-    plt.legend(loc=1)
-
-    plt.subplot(313)
-    plt.plot(t, emulator_sim.data[p_stim], 'k', label='target')
-    plt.plot(t, nengo_sim.data[p_post], label='nengo')
-    plt.plot(t, emulator_sim.data[p_post], label='emulator')
-    plt.plot(t, loihi_sim.data[p_post], label='loihi')
-    plt.legend(loc=1)
-
-    assert allclose(emulator_sim.data[p_post], nengo_sim.data[p_post],
-                    atol=0.08, rtol=0.04)
-    assert allclose(loihi_sim.data[p_post], emulator_sim.data[p_post],
-                    atol=0.1, rtol=0.05)
