4ac74cdbb3d6d2518622732dfbd64bf3eff6d71c
Aaron Voelker <arvoelke@gmail.com>
2019-03-08 17:31:10 -0500

    Test same simulation results for all allocators

--- nengo_loihi/hardware/tests/test_allocators.py
+++ nengo_loihi/hardware/tests/test_allocators.py
@@ -1,5 +1,6 @@
 import nengo
 from nengo.exceptions import BuildError, ValidationError
+from nengo.utils.numpy import rms
 import numpy as np
 import pytest
 
@@ -195,3 +196,47 @@ def test_round_robin_allocator_over():
     assert len(chip2.cores[0].synapses) == 0
     assert len(chip2.cores[0].blocks) == 0
     assert len(chip2.cores[0].inputs) == 1
+
+
+@pytest.mark.skipif(pytest.config.getoption('--target') != 'loihi',
+                    reason="Hardware allocators are Loihi-only")
+def test_deterministic_network_allocation(Simulator, seed):
+    # test that we get the same simulations results across allocators.
+    # the determinism of the allocation itself is covered by other unit tests.
+    n_neurons = 64
+    n_ensembles = 8
+    tau = 0.1
+    sim_t = 1.0
+
+    with nengo.Network(seed=seed) as model:
+        prev = nengo.Node(output=1)
+
+        p = []
+        for i in range(n_ensembles):
+            ens = nengo.Ensemble(n_neurons, 1)
+            nengo.Connection(prev, ens, synapse=tau)
+            p.append(nengo.Probe(ens, synapse=tau))
+            prev = ens
+
+    with nengo.Simulator(model) as sim_ref:
+        sim_ref.run(sim_t)
+
+    allocation = [
+        (1, OneToOne()),
+        (1, RoundRobin(n_chips=1)),
+        (3, RoundRobin(n_chips=3)),
+        (8, RoundRobin(n_chips=8)),
+    ]
+
+    sim_prev = None
+    for n_chips_used, allocator in allocation:
+        with Simulator(model, precompute=True,
+                       hardware_options={'allocator': allocator}) as sim_loihi:
+            sim_loihi.run(sim_t)
+
+        assert n_chips_used == sim_loihi.sims["loihi"].board.n_chips
+        for p_i in p:
+            assert rms(sim_loihi.data[p_i] - sim_ref.data[p_i]) < 0.05
+            if sim_prev is not None:
+                assert np.allclose(sim_prev.data[p_i], sim_loihi.data[p_i])
+        sim_prev = sim_loihi
