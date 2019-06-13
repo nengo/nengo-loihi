3392c01a36511100035200c9b5a69c8b1f119145
Eric Hunsberger <erichuns@gmail.com>
2019-06-13 17:15:39 -0400

    Fix neuron type error message

--- nengo_loihi/builder/ensemble.py
+++ nengo_loihi/builder/ensemble.py
@@ -124,7 +124,7 @@ def build_neurons(model, neurontype, neurons, block):
         "switch to a supported neuron type like LIF or "
         "SpikingRectifiedLinear, or explicitly mark ensembles using this "
         "neuron type as off-chip with\n"
-        "  net.config[ensembles].on_chip = False")
+        "  net.config[ensembles].on_chip = False" % type(neurontype).__name__)
 
 
 @Builder.register(nengo.LIF)
