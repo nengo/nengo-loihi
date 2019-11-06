b2deba76a8865fc08f55614b5695ea214a9f31b3
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2019-11-06 08:23:38 -0800

    squash! WIP: Add parallel_ensembles benchmark
    - Remove weird intercept distribution
    - Allow input and output funnelling and learning to be configured

--- docs/benchmarks/parallel_ensembles.py
+++ docs/benchmarks/parallel_ensembles.py
@@ -1,13 +1,30 @@
+import cProfile
+import timeit
+
+import matplotlib.pyplot as plt
 import nengo
 import nengo_loihi
 from nengo_loihi import decode_neurons
 import numpy as np
-import timeit
-import traceback
-
-from utils import AreaIntercepts, Triangular
 
 rng = np.random.RandomState(1)
+seed = rng.randint(0, 2**31)
+
+LoihiEmulator = lambda net: nengo_loihi.Simulator(net, target="sim")
+LoihiSimulator = lambda net: nengo_loihi.Simulator(
+    net, target="loihi", hardware_options=dict(snip_max_spikes_per_step=300),
+)
+NengoSimulator = lambda net: nengo.Simulator(net)
+
+Simulator = LoihiSimulator
+
+funnel_input = True
+funnel_output = True
+learning = True
+profile = False
+#profile = True
+
+simtime = 10
 
 #n_input = 6
 #n_output = 3
@@ -22,150 +39,123 @@ n_ensembles = 20
 #pes_learning_rate = 5e-9
 pes_learning_rate = 5e-6
 #pes_learning_rate = 5e-4
-seed = 0
 
 # synapse time constants
-tau_input = 0.01  # on input connection
-tau_training = 0.01  # on the training signal
-#tau_output = 0.2  # on the output from the adaptive ensemble
-tau_output = 0.01  # on the output from the adaptive ensemble
-# NOTE: the time constant on the neural activity used in the learning
-# connection is the default 0.005, and can be set by specifying the
-# pre_synapse parameter inside the PES rule instantiation
+tau_input = 0.01
+tau_error = 0.01
+tau_output = 0.01
 
-# set up neuron intercepts
-intercepts_bounds = [-0.3, 0.1]
-intercepts_mode = 0.1
-
-intercepts_dist = AreaIntercepts(
-    dimensions=n_input,
-    base=Triangular(intercepts_bounds[0], intercepts_mode, intercepts_bounds[1]),
-)
-intercepts = intercepts_dist.sample(n=n_neurons * n_ensembles, rng=rng)
-intercepts = intercepts.reshape(n_ensembles, n_neurons)
-
-np.random.seed = seed
 encoders_dist = nengo.dists.UniformHypersphere(surface=True)
 encoders = encoders_dist.sample(n_neurons * n_ensembles, n_input, rng=rng)
 encoders = encoders.reshape(n_ensembles, n_neurons, n_input)
 
-nengo_model = nengo.Network(seed=seed)
-nengo_model.config[nengo.Ensemble].neuron_type = nengo.LIF()
-# nengo_model.config[nengo.Connection].synapse = None
+input_freq = np.pi
+input_phases = (1 / n_input) * np.arange(n_input)
 
-with nengo_model:
+# desired transform between input and output
+assert n_input % n_output == 0
+inout_transform = np.repeat(np.eye(n_output), n_input / n_output, axis=1)
+inout_transform /= inout_transform.sum(axis=1, keepdims=True)
+
+net = nengo.Network(seed=seed)
+net.config[nengo.Ensemble].neuron_type = nengo.LIF()
+net.config[nengo.Ensemble].intercepts = nengo.dists.Uniform(-1, 0.5)
+net.config[nengo.Ensemble].max_rates = nengo.dists.Uniform(100, 200)
+# net.config[nengo.Connection].synapse = None
+
+with net:
     error_rng = np.random.RandomState(5)
 
-    assert n_input % n_output == 0
-    inout_transform = np.repeat(np.eye(n_output), n_input / n_output, axis=1)
-    inout_transform /= inout_transform.sum(axis=1, keepdims=True)
-
-    #inout_transform = error_rng.uniform(-1, 1, size=(n_output, n_input))
-    #inout_transform /= np.sqrt((inout_transform**2).sum(axis=1, keepdims=True))
-
-    def arm_func(t, u_adapt):
-        freq = np.pi
-        phases = (1 / n_input) * np.arange(n_input)
-        inputs = np.sin(freq*t + 2*np.pi*phases)
+    def ctrl_func(t, u_adapt):
+        inputs = np.sin(input_freq*t + 2*np.pi*input_phases)
 
         target_output = np.dot(inout_transform, inputs)
         errors = u_adapt - target_output
 
-        if int(1000 * t) % 1000 == 0:
-            print(("i", inputs))
-            print(("u", u_adapt))
-            print(("o", target_output))
-            print(("e", errors))
-
         return inputs.tolist() + errors.tolist()
 
-    arm = nengo.Node(arm_func, size_in=n_output, size_out=n_input+n_output, label="arm")
-    arm_probe = nengo.Probe(arm)
+    ctrl = nengo.Node(ctrl_func, size_in=n_output, size_out=n_input+n_output, label="ctrl")
+    ctrl_probe = nengo.Probe(ctrl)
 
-    input_decodeneurons = decode_neurons.Preset10DecodeNeurons()
-    onchip_input = input_decodeneurons.get_ensemble(dim=n_input)
-    nengo.Connection(arm[:n_input], onchip_input, synapse=None)
-    inp2ens_transform = np.hstack(
-        [np.eye(n_input), -np.eye(n_input)] * input_decodeneurons.pairs_per_dim
-    )
+    input = ctrl[:n_input]
+    error = ctrl[n_input:]
+    output = ctrl
 
-    output_decodeneurons = decode_neurons.Preset10DecodeNeurons()
-    onchip_output = output_decodeneurons.get_ensemble(dim=n_output)
-    out2arm_transform = np.hstack(
-        [np.eye(n_output), -np.eye(n_output)] * output_decodeneurons.pairs_per_dim
-    ) / 2000.
-    nengo.Connection(onchip_output.neurons, arm, transform=out2arm_transform, synapse=tau_output)
-
-    adapt_ens = []
-    conn_learn = []
-    for ii in range(n_ensembles):
-        adapt_ens.append(
-            nengo.Ensemble(
-                n_neurons=n_neurons,
-                dimensions=n_input,
-                intercepts=intercepts[ii],
-                radius=np.sqrt(n_input),
-                encoders=encoders[ii],
-                label="ens%02d" % ii,
-            )
+    inp2ens_transform = None
+    if funnel_input:
+        input_decodeneurons = decode_neurons.Preset10DecodeNeurons()
+        onchip_input = input_decodeneurons.get_ensemble(dim=n_input)
+        nengo.Connection(input, onchip_input, synapse=None)
+        inp2ens_transform = np.hstack(
+            [np.eye(n_input), -np.eye(n_input)] * input_decodeneurons.pairs_per_dim
         )
+        input = onchip_input
 
-        # hook up input signal to adaptive population to provide context
-        inp2ens_transform_ii = np.dot(encoders[ii], inp2ens_transform)
-        nengo.Connection(onchip_input.neurons, adapt_ens[ii].neurons,
-                         transform=inp2ens_transform_ii, synapse=tau_input)
-
-        conn_learn.append(
-            nengo.Connection(
-                adapt_ens[ii],
-                onchip_output,
-                learning_rule_type=nengo.PES(
-                    pes_learning_rate, pre_synapse=tau_training,
-                ),
-                #transform=inout_transform / n_ensembles,
-                #transform=inout_transform / n_ensembles * 0.1,
-                transform=rng.uniform(-0.01, 0.01, size=(n_output, n_input)),
-                #transform=np.zeros((n_output, n_input)),
-                #transform=np.zeros((n_output, n_neurons)),
-                #transform=np.zeros((onchip_output.n_neurons, n_neurons)),
-            )
-        )
-
-        # hook up the training signal to the learning rule
+    if funnel_output:
+        output_decodeneurons = decode_neurons.Preset10DecodeNeurons()
+        onchip_output = output_decodeneurons.get_ensemble(dim=n_output)
+        out2ctrl_transform = np.hstack(
+            [np.eye(n_output), -np.eye(n_output)] * output_decodeneurons.pairs_per_dim
+        ) / 2000.
         nengo.Connection(
-            arm[n_input:], conn_learn[ii].learning_rule,
-            synapse=None,
-            #synapse=tau_training
+            onchip_output.neurons,
+            output,
+            transform=out2ctrl_transform,
+            synapse=tau_output,
+        )
+        output = onchip_output
+
+    for ii in range(n_ensembles):
+        ens = nengo.Ensemble(
+            n_neurons=n_neurons,
+            dimensions=n_input,
+            radius=np.sqrt(n_input),
+            encoders=encoders[ii],
+            label="ens%02d" % ii,
         )
 
-import cProfile
+        if inp2ens_transform is not None:
+            inp2ens_transform_ii = np.dot(encoders[ii], inp2ens_transform)
+            nengo.Connection(
+                input.neurons,
+                ens.neurons,
+                transform=inp2ens_transform_ii,
+                synapse=tau_input
+            )
+        else:
+            nengo.Connection(input, ens, synapse=tau_input)
 
-with nengo_loihi.Simulator(
-        nengo_model,
-        target='loihi',
-        hardware_options=dict(snip_max_spikes_per_step=300)) as sim:
-# with nengo.Simulator(nengo_model) as sim:
+        conn_kwargs = dict()
+        if learning:
+            conn_kwargs["transform"] = rng.uniform(-0.01, 0.01, size=(n_output, n_input))
+            conn_kwargs["learning_rule_type"] = nengo.PES(
+                pes_learning_rate, pre_synapse=tau_error,
+            )
+        else:
+            conn_kwargs["transform"] = inout_transform / n_ensembles
 
-    hw = sim.sims["loihi"]
-    print("n_errors: %s" % hw.nengo_io_h2c_errors)
-    print("n_outputs: %s" % hw.nengo_io_c2h_count)
+        conn = nengo.Connection(ens, output, **conn_kwargs)
 
-    sim.run(0.001)
-    start = timeit.default_timer()
-    #sim.run(0.005)
-    #sim.run(1)
-    #sim.run(4)
-    sim.run(10)
-    #sim.run(30)
-    #cProfile.runctx("sim.run(1)", globals(), locals(), sort="cumtime")
-    #cProfile.runctx("sim.run(0.001)", globals(), locals(), sort="cumtime")
-    print('Run time: %0.5f' % (timeit.default_timer() - start))
-    print('Run time/step: %0.2f ms' % (1000 * sim.timers["snips"] / sim.n_steps))
+        if learning:
+            nengo.Connection(error, conn.learning_rule, synapse=None)
 
 
-    inputs = sim.data[arm_probe][:, :n_input]
+with Simulator(net) as sim:
+    sim.run(0.001)  # eliminate any extra startup from timing
+
+    steps = sim.n_steps
+    timer = timeit.default_timer()
+    if profile:
+        cProfile.runctx("sim.run(%s)" % simtime, globals(), locals(), sort="cumtime")
+    else:
+        sim.run(simtime)
+    timer = timeit.default_timer() - timer
+    steps = sim.n_steps - steps
+    print('Run time/step: %0.2f ms' % (1000 * timer / steps))
+
+    inputs = sim.data[ctrl_probe][:, :n_input]
     targets = np.dot(inputs, inout_transform.T)
-    errors = sim.data[arm_probe][:, n_input:]
+    errors = sim.data[ctrl_probe][:, n_input:]
     outputs = errors + targets
 
     error_steps = min(sim.n_steps, 2000)
@@ -173,7 +163,6 @@ with nengo_loihi.Simulator(
     end_error = np.abs(errors[-error_steps:]).mean()
     print("Error: start %0.3f, end %0.3f" % (start_error, end_error))
 
-import matplotlib.pyplot as plt
-plt.plot(sim.trange(), targets, ":")
-plt.plot(sim.trange(), outputs)
-plt.savefig("dynamics_adaptation_test.pdf")
+    plt.plot(sim.trange(), targets, ":")
+    plt.plot(sim.trange(), outputs)
+    plt.savefig("parallel_ensembles.pdf")
