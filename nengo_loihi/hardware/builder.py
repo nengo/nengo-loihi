298a8f3435b27153c25b09c33ae59b42c3313bc6
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2019-12-13 11:00:20 -0800

    Do not build unused/dummy synapses
    These are synapses with `base == None` or no weights. Building them
    not only uses extra resources, but can cause index errors in the
    NxSDK axon compiler.
    
    Fixes the "invalid index" error raised in #246.

--- nengo_loihi/hardware/builder.py
+++ nengo_loihi/hardware/builder.py
@@ -404,9 +404,9 @@ def build_synapse(nxsdk_core, core, block, synapse, compartment_idxs):  # noqa C
     synapse_map = {}  # map weight_idx to (ptr, pop_size, len)
     total_synapse_ptr = int(core.synapse_entries[synapse][0])
     for axon_idx, axon_id in enumerate(axon_ids):
-        assert axon_id <= 2 ** axon_bits
+        assert axon_id is None or axon_id <= 2 ** axon_bits
 
-        weight_idx = int(synapse.axon_weight_idx(axon_idx))
+        weight_idx = synapse.axon_weight_idx(axon_idx)
         base = synapse.axon_compartment_base(axon_idx)
 
         if weight_idx not in synapse_map:
@@ -440,17 +440,12 @@ def build_synapse(nxsdk_core, core, block, synapse, compartment_idxs):  # noqa C
         synapse_ptr, n_atoms, n_compartments = synapse_map[weight_idx]
         assert n_atoms <= 2 ** atom_bits
 
-        if base is None:
-            # This is a dummy axon with no weights. We set `n_compartments = 0`,
-            # but with population axons it appears the axon can still have an effect.
-            # So we also make sure not to connect into it with incoming axons. However,
-            # we still build the axon here so that our indexing is not thrown off.
-            synapse_ptr = 0
-            n_compartments = 0
-            base = 0
-        else:
-            base = int(base)
+        if axon_id is None:
+            # This is a dummy axon with no base or no weights, so skip it
+            assert base is None or n_compartments == 0
+            continue
 
+        base = int(base)
         assert base <= d(b"MjU2", int), "Currently limited by hardware"
         d_set(
             d_get(nxsdk_core, b"c3luYXBzZU1hcA==")[axon_id],
@@ -531,7 +526,7 @@ def build_synapse(nxsdk_core, core, block, synapse, compartment_idxs):  # noqa C
 
 def build_axons(nxsdk_core, core, block, axon, compartment_ids, pop_id_map):
     synapse = axon.target
-    tchip_idx, tcore_idx, tsyn_idxs = core.board.find_synapse(synapse)
+    tchip_idx, tcore_idx, taxon_ids = core.board.find_synapse(synapse)
     nxsdk_board = d_get(nxsdk_core, b"cGFyZW50", b"cGFyZW50")
     tchip_id = d_get(d_get(nxsdk_board, b"bjJDaGlwcw==")[tchip_idx], b"aWQ=")
     tcore_id = d_get(
@@ -548,11 +543,12 @@ def build_axons(nxsdk_core, core, block, axon, compartment_ids, pop_id_map):
         if spike is None:
             continue  # this compartment does not route through these axons
 
-        taxon_idx = int(spike.axon_id)
-        taxon_id = int(tsyn_idxs[taxon_idx])
+        taxon_idx = spike.axon_idx
+        taxon_id = taxon_ids[taxon_idx]
         atom = int(spike.atom)
         n_atoms = synapse.axon_populations(taxon_idx)
-        if synapse.axon_compartment_base(taxon_idx) is None:
+
+        if taxon_id is None:
             continue  # this connects to a dummy axon, so do not build
 
         if synapse.pop_type == 0:  # discrete
--- nengo_loihi/hardware/nxsdk_objects.py
+++ nengo_loihi/hardware/nxsdk_objects.py
@@ -103,7 +103,11 @@ class Core:
         self.stdp_pre_cfgs = []
 
         self.synapse_cfg_idxs = {}  # one synfmt per Synapse, for now
+
+        # for each Synapse, provides a map from axon index to axon id
         self.synapse_axons = collections.OrderedDict()
+
+        # for each Synapse, provides the indices occupied in the synapse weight table
         self.synapse_entries = collections.OrderedDict()
 
         self.learning_coreid = None
@@ -169,15 +173,30 @@ class Core:
         synapse_cfg_idx = self.get_synapse_cfg_idx(synapse.synapse_cfg)
         self.synapse_cfg_idxs[synapse] = synapse_cfg_idx
 
-        a0 = 0
+        # determine starting ID for this synapse's axons
+        id0 = 0
         if len(self.synapse_axons) > 0:
             last = next(reversed(self.synapse_axons))
-            a0 = self.synapse_axons[last][-1] + 1
-        idxs_per_synapse = synapse.idxs_per_synapse()
-        idxs = [a0 + idxs_per_synapse * i for i in range(synapse.n_axons)]
-        self.synapse_axons[synapse] = idxs
-        self.board.index_synapse(synapse, self.chip, self, idxs)
+            id0 = self.synapse_axons[last][-1] + 1
 
+        # determine the ID for each synapse axon index
+        idxs_per_synapse = synapse.idxs_per_synapse()
+        i = id0
+        ids = []
+        for idx in range(synapse.n_axons):
+            base = synapse.axon_compartment_base(idx)
+            w, _ = synapse.axon_weights_indices(idx)
+            if base is None or w.size == 0:
+                # dummy axon, which we will not build
+                ids.append(None)
+            else:
+                ids.append(i)
+                i += idxs_per_synapse
+
+        self.synapse_axons[synapse] = ids
+        self.board.index_synapse(synapse, self.chip, self, ids)
+
+        # determine the indices in the synapse weight table that this synapse occupies
         s0 = 0
         if len(self.synapse_entries) > 0:
             last = next(reversed(self.synapse_entries))
@@ -294,7 +313,7 @@ class LoihiSpikeInput:
         for axon in spike_input.axons:
             synapse = axon.target
             atom_bits_extra = synapse.atom_bits_extra()
-            tchip_idx, tcore_idx, tsyn_ids = board.find_synapse(synapse)
+            tchip_idx, tcore_idx, taxon_ids = board.find_synapse(synapse)
             tchip = d_get(nxsdk_board, b"bjJDaGlwcw==")[tchip_idx]
             tcore = d_get(tchip, b"bjJDb3Jlcw==")[tcore_idx]
             spikes = axon.map_spikes(input_idxs)
@@ -305,8 +324,8 @@ class LoihiSpikeInput:
                     continue  # pragma: no cover
 
                 self.axon_map.setdefault(input_idx, [])
-                taxon_idx = int(spike.axon_id)
-                if synapse.axon_compartment_base(taxon_idx) is None:
+                taxon_id = taxon_ids[spike.axon_idx]
+                if taxon_id is None:
                     continue  # this goes to a dummy axon, so do not connect
 
                 self.axon_map[input_idx].append(
@@ -316,7 +335,7 @@ class LoihiSpikeInput:
                             axon.pop_type,
                             tchip.id,
                             tcore.id,
-                            int(tsyn_ids[taxon_idx]),
+                            taxon_id,
                             spike.atom,
                             atom_bits_extra,
                         ),
