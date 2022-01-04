1ae3fc59f1858431c3dccd21be3e54ab16d695d4
Eric Hunsberger <eric.hunsberger@appliedbrainresearch.com>
2022-01-04 15:26:17 -0500

    fixup! Add docs/examples/decode_neurons.py

--- docs/examples/decode_neurons.py
+++ docs/examples/decode_neurons.py
@@ -4,34 +4,34 @@ import numpy as np
 
 import nengo_loihi
 
-n_neurons = 500  # number of neurons in on-chip ensemble
+n_neurons = 250  # number of neurons in on-chip ensemble
 input_d = 2  # number of input dimensions
 dt = 0.001
+
+encode_neuron_type = nengo_loihi.decode_neurons.OnOffDecodeNeurons(dt=dt, is_input=True)
+
+weights = nengo.dists.UniformHypersphere(surface=True).sample(n_neurons, d=input_d)
 
 with nengo.Network() as net:
     nengo_loihi.set_defaults()
     nengo_loihi.add_params(net)
 
+    # input node
     f = 2 * np.pi
     inp = nengo.Node(lambda t: [np.sin(f * t), np.cos(f * t)])
     assert input_d == inp.size_out
 
-    encode_neuron_type = nengo_loihi.decode_neurons.OnOffDecodeNeurons(dt=dt)
+    # off-chip ensemble to turn input into spikes
     encode_ens = encode_neuron_type.get_ensemble(input_d)
     net.config[encode_ens].on_chip = False
-    nengo.Connection(inp, encode_ens)
+    nengo.Connection(inp, encode_ens, synapse=None)
 
-    weights = nengo.dists.UniformHypersphere().sample(n_neurons, input_d)
+    # on-chip ensemble to connect input to using `weights`
     onchip_ens = nengo.Ensemble(n_neurons, input_d, encoders=weights)
+    onchip_weights = dt * encode_neuron_type.get_post_encoders(weights).T
+    nengo.Connection(encode_ens.neurons, onchip_ens.neurons, transform=onchip_weights)
 
-    nengo.Connection(
-        encode_ens.neurons,
-        onchip_ens.neurons,
-        transform=encode_neuron_type.get_post_encoders(weights).T,
-    )
-
-    p = nengo.Probe(onchip_ens, synapse=0.01)
-
+    p = nengo.Probe(onchip_ens, synapse=nengo.Alpha(0.01))
 
 with nengo_loihi.Simulator(net) as sim:
     sim.run(2.0)
